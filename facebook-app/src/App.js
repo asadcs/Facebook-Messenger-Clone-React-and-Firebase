571ed92f2592a9698cd4e47bc20fe7f142678d05
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-14 14:31:47 -0700

    Update App.js

--- facebook-app/src/App.js
+++ facebook-app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const ValueHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    setMessages([...messages, input]);
+    console.log(messages);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <form>
+     <h1>Facebook Messenger Clone React and Firebase</h1>
+      <input value={input} onChange={ValueHandler}></input>
+      <button text="Submit" type="submit" onClick={onSubmitHandler}>
+        send message
+      </button>
+     </form>
+
+    
+        {messages.map((message,idx) => (
+          <p key={idx}>{message}</p>
+        ))}
+      
     </div>
   );
 }
