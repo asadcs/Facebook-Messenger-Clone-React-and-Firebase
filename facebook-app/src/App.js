4e0f1047e2499108e28d0d15e9ec2a34b288fbac
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-14 14:45:50 -0700

    Material UI installed and used

--- facebook-app/src/App.js
+++ facebook-app/src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 import { useState } from "react";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+} from "@material-ui/core";
 
 function App() {
   const [input, setInput] = useState("");
@@ -10,7 +18,7 @@ function App() {
   };
 
   const onSubmitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setMessages([...messages, input]);
     console.log(messages);
     setInput("");
@@ -18,20 +26,36 @@ function App() {
 
   return (
     <div className="App">
-     
-     <form>
-     <h1>Facebook Messenger Clone React and Firebase</h1>
-      <input value={input} onChange={ValueHandler}></input>
-      <button text="Submit" type="submit" onClick={onSubmitHandler}>
-        send message
-      </button>
-     </form>
+      <form>
+        <h1>Facebook Messenger Clone React and Firebase</h1>
+        <FormControl>
+          <InputLabel htmlFor="my-input">message</InputLabel>
+          <Input
+            id="my-input"
+            aria-describedby="my-helper-text"
+            value={input}
+            onChange={ValueHandler}
+          />
+          <Button
+            type="submit"
+            onClick={onSubmitHandler}
+            variant="contained"
+            color="primary"
+            text="send message"
+            disabled={!input}
+          >
+            
+          </Button>
+        </FormControl>
+        {/* <input value={input} onChange={ValueHandler}></input> */}
+        {/* <Button text="Submit" type="submit" onClick={onSubmitHandler}>
+        
+      </Button> */}
+      </form>
 
-    
-        {messages.map((message,idx) => (
-          <p key={idx}>{message}</p>
-        ))}
-      
+      {messages.map((message, idx) => (
+        <p key={idx}>{message}</p>
+      ))}
     </div>
   );
 }
